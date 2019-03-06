da9d6d81266fe0b301665aae021134c46b23afe9
Angel <angel@Users-Mac.local>
2019-03-06 13:34:39 -0800

    Local pods

---
+++ build/Pods.build/Debug-iphonesimulator/Pods-MeasureARPusher.build/DerivedSources/Pods_MeasureARPusher_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_MeasureARPusherVersionString[];
+ extern const double Pods_MeasureARPusherVersionNumber;
+
+ const unsigned char Pods_MeasureARPusherVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MeasureARPusher  PROJECT:Pods-1" "\n";
+ const double Pods_MeasureARPusherVersionNumber __attribute__ ((used)) = (double)1.;
